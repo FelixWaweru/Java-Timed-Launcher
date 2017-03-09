eb48ba28c0023a0bcea80125f91bbd966bee2a0f
FelixWaweru <wawerufelix60@gmail.com>
2017-03-09 19:03:42 +0300

    Updated code with statements

--- boredom/src/boredom/Boredom.java
+++ boredom/src/boredom/Boredom.java
@@ -16,29 +16,38 @@ import java.util.Date;
 public class Boredom {
     
     public static void main(String[] args) throws IOException, InterruptedException, AWTException {
+        //variables that will be used to create an infinite loop
         int i=0;
         int j=1;
-        Boredom b = new Boredom();
         DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        //infinite loop is instantiated
         while(i!=j){
+         //one second delay to countdown the time in seconds
         Thread.sleep(1000);
+        //user defined countdown time
         String bb="20:21:00";
-        Date a= new Date();       
+        //current time is recorded
+        Date a= new Date();
+        //date is converted into a string format similar to the one that is input by the user
         String datee= dateFormat.format(a);
         System.out.println(datee);
+        //conditional statement to compare current time to user defined countdown time
         if(datee.compareTo(bb)==0){
         System.out.println("Yaay");
-          Toolkit.getDefaultToolkit().beep();
+        //Mailing function that will send email to notify one of the function beginning
           Runtime.getRuntime().exec("cmd /c start chrome.exe https://mail.google.com/mail/u/0/#inbox?compose=15aae6072fc45aa7");
           Thread.sleep(15000);
+          //robot function that will simulate the pressing of the enter button to send the email
           Robot r = new Robot();
           r.keyPress(KeyEvent.VK_ENTER);
           r.keyRelease(KeyEvent.VK_ENTER);
+          //series of applications and what they will do is instantiated
           Runtime.getRuntime().exec("cmd /c start chrome.exe www.youtube.com/watch?v=sDKPtObKlcw?vq=medium");
           Runtime.getRuntime().exec("cmd /c start chrome.exe www.youtube.com/watch?v=jG7dSXcfVqE?vq=hd720");
           Runtime.getRuntime().exec("cmd /c start chrome.exe www.youtube.com/watch?v=DxphJ-dnX2Y?vq=hd720");
           Runtime.getRuntime().exec("cmd /c start chrome.exe www.youtube.com/watch?v=AORsw8NpN4E?vq=hd720");
           Runtime.getRuntime().exec("cmd /c start chrome.exe www.youtube.com/watch?v=mwAoDLyJaZ4?vq=hd720");
+          //loop breaks
           break;
         }
         }
